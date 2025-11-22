caee73dc410cc6bb0c68b7c47a7855f0f71d1553
Raj <rajendra22wagh@gmail.com>
2025-11-22 22:10:24 +0530

    First Commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  cloud {
+    organization = "Raj-Demo-Organization"
+    workspaces {
+      name = "raj-tfc-demo"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "tfc-cli-demo-raj-bucket-123"
+}
